823843e6f758ed5002cfb268539d55d91da127bd
Oliver Klemenz <oklemenz@web.de>
2022-07-04 09:54:01 +0200

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from 'https://unpkg.com/three@0.142.0/examples/jsm/controls/OrbitControls.js';
+import { RoomEnvironment } from 'https://unpkg.com/three@0.142.0/examples/jsm/environments/RoomEnvironment.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.142.0/examples/jsm/loaders/GLTFLoader.js';
+
+export function init() {
+  const container = document.getElementById("container");
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  container.appendChild(renderer.domElement);
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+  const scene = new THREE.Scene();
+  scene.background = null; // transparent
+  scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
+
+  const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+  camera.rotation.y = 45 / 180 * Math.PI;
+  camera.position.x = 800;
+  camera.position.y = 100;
+  camera.position.z = 1000;
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+  controls.enablePan = false;
+  controls.enableDamping = true;
+
+  const loader = new GLTFLoader();
+  loader.load("models/scene.gltf", function (gltf) {
+    const car = gltf.scene.children[0];
+    car.scale.set(0.5, 0.5, 0.5);
+    scene.add(gltf.scene);
+    animate();
+  }, undefined, function (err) {
+    console.error(e);
+  });
+
+  function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+  }
+
+  window.onresize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+}
+
+init();
